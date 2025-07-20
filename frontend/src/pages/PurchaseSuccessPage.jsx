d10e0bbbefe7ac209200b31c0c83680a4d5543a2
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-21 02:48:51 +0300

    remove logs

--- frontend/src/pages/PurchaseSuccessPage.jsx
+++ frontend/src/pages/PurchaseSuccessPage.jsx
@@ -24,7 +24,6 @@ const PurchaseSuccessPage = () => {
 		};
 
 		const sessionId = new URLSearchParams(window.location.search).get("session_id");
-		console.log(sessionId)
 		if (sessionId) {
 			handleCheckoutSuccess(sessionId);
 		} else {
